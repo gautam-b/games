0371320c04f2ee29425873a02ce251cf0e1d3fcb
Gautam <30121546+gautam-b@users.noreply.github.com>
2020-05-01 18:38:55 +0530

    minor changes

--- soduku_solver/Soduku.py
+++ soduku_solver/Soduku.py
@@ -17,13 +17,13 @@ def find_cell_to_fill(sudoku):
     for i in range(9):
         for j in range(9):
             if sudoku[i][j] == 0:
-                return i, j
-    return ("Solved", 0)
+                return (i, j)
+    return False
 
 
 def is_valid(sudoku, row_num, col_num, number):
     if number not in sudoku[row_num]:  # check row
-        if number not in [row[col_num] for row in sudoku]:  # check row
+        if number not in [row[col_num] for row in sudoku]:  # check col
             region_row_start = 3 * (row_num // 3)
             region_col_start = 3 * (col_num // 3)
             for i in range(region_row_start, region_row_start + 3):
@@ -36,11 +36,12 @@ def is_valid(sudoku, row_num, col_num, number):
 
 def brute_force(sudoku):
     global backtracks
-    i, j = find_cell_to_fill(sudoku)
-    if i == "Solved":
+    cell = find_cell_to_fill(sudoku)
+    if not cell:
         print("Solved")
         return True
 
+    i, j = cell
     for number in range(1, 10):
         if is_valid(sudoku, i, j, number):
             sudoku[i][j] = number

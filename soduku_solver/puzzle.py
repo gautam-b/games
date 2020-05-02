9d540558f1ee97214b8b44df2f4143adffbaeb36
Saiem Irfan <Saiem.irfan.2000@gmail.com>
2020-05-02 18:03:09 -0400

    Generates a board using online board

--- soduku_solver/puzzle.py
+++ soduku_solver/puzzle.py
@@ -1,3 +1,8 @@
+from bs4 import BeautifulSoup, element
+import requests
+from typing import List, Optional
+import urllib.request
+
 diabolical = [
     [4, 0, 7, 0, 0, 0, 6, 0, 0],
     [9, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -106,3 +111,74 @@ diff = [
     [0, 0, 4, 0, 0, 0, 0, 3, 0],
     [0, 0, 0, 0, 0, 9, 7, 0, 0],
 ]
+
+#TODO Somehow fix this non-randomness
+#id 1 = 6, 7 = 2
+
+def format_html() -> element.ResultSet:
+    default_gateway = "https://testdrive-archive.azurewebsites.net/Performance/Sudoku/Default.html"
+
+    html = requests.get(default_gateway).text
+    soup = BeautifulSoup(html, 'lxml')
+    board_cells = soup.find_all('td', class_="boardCell")
+    return board_cells
+
+
+def html_to_array(board_cells: element.ResultSet) -> List[Optional[int]]:
+    """ Returns the board_cells html data into one large array-like list of integers """
+    cell_possibilities = [None for i in range(89)]
+    for cell in board_cells:
+        have_value = cell.find('div', class_="staticValue")
+        if not have_value:
+            have_value = cell.find('div', class_="editValue")
+            value = 0
+        else:
+            value = int(cell.span.text)
+        cell_possibilities[int(have_value.attrs['id'])] = value
+
+    cell_singular = [i for i in cell_possibilities if i is not None]
+    return cell_singular
+
+
+def array_to_final_board(input_list: list) -> List[List[int]]:
+    """ Returns the final board using output from html_to_array
+
+    Precondition: <input_list> is of length 81
+    """
+    temp_final_list = []
+
+    for i in range(9):
+        row_list = list()
+        for j in range(9):
+            row_list.append(input_list[i*9 + j])
+        temp_final_list.append(row_list)
+
+    return fix_board(temp_final_list)
+
+
+def fix_board(board_broken: List[int]) -> List[int]:
+    """ Returns a transposed version of board_broken
+
+    Precondition: <board_broken> is a list with 9 nested lists of size 9
+    """
+    final_board = list()
+    for big_row_partition in range(3):
+        row_1, row_2, row_3 = [], [], []
+        for row_selection in range(3):
+            row = board_broken[(big_row_partition * 3) + row_selection]
+            row_1.extend(row[:3])
+            row_2.extend(row[3:6])
+            row_3.extend(row[6:])
+
+        final_board.append(row_1)
+        final_board.append(row_2)
+        final_board.append(row_3)
+
+    return final_board
+
+daily = array_to_final_board(html_to_array(format_html()))
+
+
+if __name__ == '__main__':
+    for i in range(9):
+        print(daily[i])
